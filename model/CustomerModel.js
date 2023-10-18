5029682520ff99848bc92dac0c1dda06d3f84bb8
thisarakavishka <thisarakavishka0@gmail.com>
2023-10-19 00:59:21 +0530

    refactor CustomerModel.js

---
+++ model/CustomerModel.js
@@ -0,0 +1,8 @@
+export class CustomerModel {
+    constructor(customer_id, first_name, last_name, address) {
+        this.customer_id = customer_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+    }
+}
