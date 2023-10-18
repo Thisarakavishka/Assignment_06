0e99045cc323b2f90af6b92db1fc232bd551501e
thisarakavishka <thisarakavishka0@gmail.com>
2023-10-19 01:04:29 +0530

    refactor ItemModel.js

---
+++ model/ItemModel.js
@@ -0,0 +1,8 @@
+export class ItemModel {
+    constructor(item_id, description, unit_price, stock) {
+        this.item_id = item_id;
+        this.description = description;
+        this.unit_price = unit_price;
+        this.stock = stock;
+    }
+}
